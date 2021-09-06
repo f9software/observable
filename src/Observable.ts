57bce3cbbbec89516b16bb19cc4babc4d9d0f16f
Iulian Ilea <iulian.ilea@gmail.com>
2021-09-06 17:09:16 +0300

    Add support for async listeners.

--- src/Observable.ts
+++ src/Observable.ts
@@ -1,8 +1,10 @@
 export interface ObservableInterface {}
 
 export interface Listener<O extends Observable> {
-	(observable: O, ...args: any[]): false | void;
+	(observable: O, ...args: any[]): ListenerResult;
 }
+
+export type ListenerResult = false | void | Promise<false | void>;
 
 export abstract class Observable implements ObservableInterface {
     private listeners: Map<string, Set<Listener<this>>> = new Map();
@@ -22,13 +24,43 @@ export abstract class Observable implements ObservableInterface {
     	this.listeners.get(eventName)?.delete(listener);
     }
 
-    fireEvent(eventName: string, ...args: any[]): false | void {
-    	const listeners = Array.from(this.listeners.get(eventName) || []);
+    fireEvent(eventName: string, ...args: any[]): ListenerResult {
+		const promises: Promise<false | void>[] = [];
+		const listeners = Array.from(this.listeners.get(eventName) || []);
 
-    	for (let i = 0; i < listeners.length; i++) {
-    		if (listeners[i](this, ...args) === false) {
-    			return false;
-    		}
-    	}
+		let rs: ListenerResult;
+		for (let i = 0; i < listeners.length; i++) {
+			rs = listeners[i](this, ...args);
+
+			if (rs instanceof Promise) {
+				// if the result is a promise, it means we're dealing with a async listener and we don't
+				// have the response right way, so we push the push the response into a promises array
+				// and will handle it later
+				promises.push(rs);
+			} else if (rs === false) {
+				// in case a synchronuous listener returns false, then stop calling any other listeners
+				// and directly return the response
+				return false;
+			}
+		}
+
+		if (promises.length > 0) {
+			// if we have promises, then return a promise back
+			// make sure to return as soon as any of the promises in the array is rejected
+			// or if the result of any promise is false
+			return Promise
+				.all(promises)
+				.then(
+					(responses) => {
+						for (let i = 0; i < responses.length; i++) {
+							if (responses[i] === false) {
+								return false;
+							}
+						}
+					},
+					// return false in case of rejection
+					() => false
+				);
+		}
     }
 }
